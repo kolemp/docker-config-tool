48a25255be9a39155bbde92fc57082f0fd752620
Przemyslaw Koltermann <kolemp@gmail.com>
2019-07-04 23:59:13 +0200

    Generate Image Name based on yaml config

--- main.go
+++ main.go
@@ -7,7 +7,11 @@ import (
 	"strings"
 	"gopkg.in/yaml.v2"
 	"bytes"
+	"flag"
+	"io/ioutil"
 )
+
+var configFileName string
 
 type DockerImageRef struct {
 	Repository  string
@@ -17,8 +21,6 @@ type DockerImageRef struct {
 
 func readConfig(source string) DockerImageRef {
 	var config DockerImageRef
-
-	fmt.Printf("%s", source)
 
 	err := yaml.Unmarshal([]byte(source), &config)
 
@@ -58,15 +60,48 @@ func parse(source string) string {
 	return tpl.String()
 }
 
+func read (filename string) string {
+	file, err := os.Open(filename)
+    if err != nil {
+        panic(err)
+    }
+
+	defer file.Close()
+	b, err := ioutil.ReadAll(file)
+	return string(b)
+}
+
+
+func init() {
+	flag.StringVar(&configFileName, "c", "", "config file path")
+}
+
 func main() {
-	os.Setenv("NAME", "gopher")
-	raw := `
-repository: "{{ getenv "FIRSTNAME" "cos" }}"
-path: "kolemp/gct"
-tag: a123
+
+	flag.Parse()
+
+	if (len(flag.Args()) == 0) {
+		fmt.Println("Yoou need to pass operation. Currently supported are: imageName")
+		os.Exit(1)
+	}
+
+	if (configFileName == "") {
+		raw := `
+repository: "{{ getenv "REPO_hostname" }}"
+path: "image/path"
+tag: "imageTag"
 `
+		fmt.Printf("You need to pass yaml config file with structure similar to this: \n %s", raw)
+		os.Exit(1)
+	}
+	raw := read(configFileName)
 	parsed := parse(raw)
 	ref := readConfig(parsed)
 
-	fmt.Printf(render(ref))
+
+	operation := flag.Args()[0]
+	if (operation == "imageName") {
+
+		fmt.Printf(render(ref))
+	}
 }
